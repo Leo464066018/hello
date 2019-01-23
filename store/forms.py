40a3e5cc6afad4b84857ad899dda9c7576f147ed
Leo464066018 <464066018@qq.com>
2019-01-23 17:05:07 +0800

    写操作模块

---
+++ store/forms.py
@@ -0,0 +1,58 @@
+from django import forms
+from django.contrib.auth import authenticate, login
+from .models import Operation, Item
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        label="用户名",
+        max_length=12,
+        min_length=3,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '请输入用户名', })
+        )
+    password = forms.CharField(
+        label="密码", 
+        max_length=12, 
+        min_length=3, 
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': '请输入密码', })
+        )
+
+    def clean(self):
+        username = self.cleaned_data['username']
+        password = self.cleaned_data['password']
+
+        user = authenticate(username=username, password=password)
+        if user is None:
+            raise forms.ValidationError('用户名或密码不正确')
+        else:
+            self.cleaned_data['user'] = user
+        return self.cleaned_data
+
+
+# class OperateForm(forms.Form):
+#     item = forms.ModelChoiceField(
+#         queryset=Item.objects.all(),
+#         label=False,
+#         empty_label='请选择配件'
+#     )
+class OperateForm(forms.ModelForm):
+    class Meta:
+        model = Operation
+        fields = ('item', 'operate', 'num')
+        widgets = {
+            'item': forms.Select(
+                attrs={
+                    'class': 'form-control',
+                }
+            ),
+            'operate': forms.Select(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'num': forms.NumberInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+        }
