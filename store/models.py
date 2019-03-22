e2d4d738a5b64a96173c05f88d874e4e6927ef1b
Leo464066018 <464066018@qq.com>
2019-03-22 11:37:05 +0800

    修改postManage的Integer为Char

--- store/models.py
+++ store/models.py
@@ -54,7 +54,7 @@ class Operation(models.Model):
 class PostManage(models.Model):
     SERVERTYPES = (('wifi', 'wifi服务器'), ('video', '视频服务器'))
     SENDORRECEIVE = (('send', '发货'), ('receive', '回收'))
-    post_num = models.IntegerField(verbose_name='单号')
+    post_num = models.CharField(max_length=12, verbose_name='单号')
     post_date = models.DateField(verbose_name='发货日期')
     post_type = models.CharField(max_length=8, choices=SENDORRECEIVE, default='send', verbose_name='收寄类型')
     server_type = models.CharField(max_length=20, choices=SERVERTYPES, verbose_name='型号')
